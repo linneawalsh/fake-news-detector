d6b01d24aeed875b6a3fb341044ba203b435c3f7
Linnea Walsh <50059771+linneawalsh@users.noreply.github.com>
2023-03-25 18:27:34 -0700

    Create model

--- detect.py
+++ detect.py
@@ -1,16 +1,18 @@
 import pandas as pd
-import numpy as np
 import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
 
+# convert DataFrame of features to bag of words model
 def format_features(unformattedFeatures):
    ps = PorterStemmer()
    corpus = []
    for i in range(0, len(unformattedFeatures)):
-      #review = re.sub('[^a-zA-Z]', ' ', unformattedFeatures.iloc[i, 0] + " " + unformattedFeatures.iloc[i, 2])
       review = re.sub('[^a-zA-Z]', ' ', unformattedFeatures.iloc[i, 0])
       review = review.lower()
       review = review.split()
@@ -19,28 +21,37 @@ def format_features(unformattedFeatures):
       review = ' '.join(review)
       corpus.append(review)
 
-   cv = CountVectorizer(min_df=2)
+   cv = CountVectorizer(min_df=5)
    feat = cv.fit_transform(corpus).toarray()
    return feat, cv
 
-def format_labels(unformattedLabels):
-   newLabels = np.zeros((len(unformattedLabels), 2))
-   for i in range(0, len(unformattedLabels)):
-      newLabels[i, unformattedLabels.iat[i]] = 1
-   return pd.DataFrame(data=newLabels, index=unformattedLabels.index)
-
-data = pd.read_csv("data.csv", index_col=0)
+data = pd.read_csv("cleanData.csv", index_col=0)
 # data from https://www.kaggle.com/competitions/fake-news/overview
+# then unneccesary columns removed
 # 0 = reliable, 1 = unreliable
 
 data = data.dropna()
 
 features = data.iloc[:,:-1]
-labels = data.iloc[:,-1]
-
 feat, cv = format_features(features)
-lab = format_labels(labels)
 
-X_train, X_test, y_train, y_test = train_test_split(feat, lab, test_size = 0.1, random_state=21)
+lab = data.iloc[:,-1]
 
-print(cv.get_feature_names_out()[0:20])
+X_train, X_test, y_train, y_test = train_test_split(feat, lab, test_size = 0.1)
+
+model = Sequential([
+   Dense(250, activation='relu'),
+   Dropout(0.3),
+   Dense(50, activation='relu'),
+   Dropout(0.3),
+   Dense(10, activation='relu'),
+   Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=5, verbose=2)
+
+loss, acc = model.evaluate(X_test, y_test, verbose=0)
+
+print("Accuracy on test set: " + str(acc*100))
